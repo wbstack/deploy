403624ea9fc84a6ff366dc68b26f4011f207301e
addshore <addshorewiki@gmail.com>
2021-06-25 14:37:39 +0100

    mediawiki134: Undeploy, destroy and delete
    https://github.com/wbstack/deploy/issues/4

--- k8s/cmd/bash-mw134-backend.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-PODNAME=$(kubectl get pods -l app.kubernetes.io/name=mediawiki,app.kubernetes.io/instance=mediawiki-134,app.kubernetes.io/component=app-backend -o jsonpath="{.items[0].metadata.name}")
-kubectl exec -it $PODNAME -- //bin/bash
