93a82e32b948e989f21c75941f3352c191b3f6a6
addshore <addshorewiki@gmail.com>
2022-01-14 16:05:12 +0000

    wbstackify storage check

--- k8s/cmd/storage-check.sh
+++ k8s/cmd/storage-check.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
+WBSTACK_KUBECTL="kubectl --context=gke_wbstack_us-east1-b_cluster-1"
+
 echo "Elastic Search"
-PODNAME=$(kubectl get pods -l release=elasticsearch -l statefulset.kubernetes.io/pod-name=elasticsearch-master-0 -o jsonpath="{.items[0].metadata.name}")
-kubectl exec -it $PODNAME -- du -sh //usr/share/elasticsearch/data
+PODNAME=$($WBSTACK_KUBECTL get pods -l release=elasticsearch -l statefulset.kubernetes.io/pod-name=elasticsearch-master-0 -o jsonpath="{.items[0].metadata.name}")
+$WBSTACK_KUBECTL exec -it $PODNAME -- du -sh //usr/share/elasticsearch/data
 
 echo "Query Service"
-PODNAME=$(kubectl get pods -l app.kubernetes.io/name=queryservice -o jsonpath="{.items[0].metadata.name}")
-kubectl exec -it $PODNAME -- du -sh //wdqs/data
-kubectl exec -it $PODNAME -- ls -lahr //wdqs/data | grep data
+PODNAME=$($WBSTACK_KUBECTL get pods -l app.kubernetes.io/name=queryservice -o jsonpath="{.items[0].metadata.name}")
+$WBSTACK_KUBECTL exec -it $PODNAME -- du -sh //wdqs/data
+$WBSTACK_KUBECTL exec -it $PODNAME -- ls -lahr //wdqs/data | grep data
 
 echo "SQL Master"
-PODNAME=$(kubectl get pods -l release=sql,component=master -o jsonpath="{.items[0].metadata.name}")
-kubectl exec -c mariadb -it $PODNAME -- du -sh //bitnami/mariadb
+PODNAME=$($WBSTACK_KUBECTL get pods -l release=sql,component=master -o jsonpath="{.items[0].metadata.name}")
+$WBSTACK_KUBECTL exec -c mariadb -it $PODNAME -- du -sh //bitnami/mariadb
 
 echo "SQL Replica"
-PODNAME=$(kubectl get pods -l release=sql,component=slave -o jsonpath="{.items[0].metadata.name}")
-kubectl exec -c mariadb -it $PODNAME -- du -sh //bitnami/mariadb
-
-
+PODNAME=$($WBSTACK_KUBECTL get pods -l release=sql,component=slave -o jsonpath="{.items[0].metadata.name}")
+$WBSTACK_KUBECTL exec -c mariadb -it $PODNAME -- du -sh //bitnami/mariadb
