66f3c2d0fd37d988e7e50f8be3b3af1c07e84fd4
addshore <addshorewiki@gmail.com>
2022-08-08 14:31:36 +0200

    Update DNS for wbstack.com github pages

--- gce/dns/push.sh
+++ gce/dns/push.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 # Updates the DNS zone based on the ./*-zone files in this repo
-gcloud dns record-sets import ./zone-opencura --zone=opencura-zone --delete-all-existing
-gcloud dns record-sets import ./zone-wbstack --zone=wbstack-zone --delete-all-existing
+gcloud --project=wbstack dns record-sets import ./zone-opencura --zone=opencura-zone --delete-all-existing
+gcloud --project=wbstack dns record-sets import ./zone-wbstack --zone=wbstack-zone --delete-all-existing
